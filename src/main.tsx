6033621499075ffa3c18c4137bd6de1c681f1438
shahbazkhaliddev786 <shahbazkhalid818@gmail.com>
2026-02-08 03:13:03 +0500

    feat: screaming/feature architecture setup

--- src/main.tsx
+++ src/main.tsx
@@ -16,4 +16,7 @@ createRoot(document.getElementById('root')!).render(
    and also create a git tag with the same version.
 
 2. Versions, lint-staged, eslint, commitlint, husky, prettier, stylelint.
+3. Screaming/Feature Architecture - The art of folder structure. This architecture is scalable and maintainable. It is based on the principle of separation of concerns. Each feature is a folder that contains all the files related to that feature. This way, we can easily find and modify the code related to a specific feature without affecting other features.
+   Application will be robust and readable codebase for developers.
+
 */
---
+++ src/shared/config/env.ts
@@ -0,0 +1,1 @@
+// Import and export all environment variables from here
